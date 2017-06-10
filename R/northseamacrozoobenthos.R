5b88e5591c061c6d9f314ad33496efdffa240174
janhoo <jm.smith@gmx.net>
2017-06-10 09:02:38 +0200

    more funcions plus data

---
+++ R/northseamacrozoobenthos.R
@@ -0,0 +1,10 @@
+#' @name northseamacrozoobenthos
+#' @title mix of taxonomic, vernacular and trivial names
+#' @description for tests and demonstration. 
+#' @docType data
+#' @usage data(northseamacrozoobenthos)
+#' @seealso \code{\link{wormsbynames}}, \code{\link{wormsbymatchnames}}
+#' @format a vector of class \code{character}.
+#' @source various
+#' @author Jan M Holstein , 2017-03-05
+NULL
